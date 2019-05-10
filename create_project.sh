d5c214ba3d7034027dacff5898003fc3e2c46591
yuecazhu <yuecazhu@bell.corp.bce.ca>
2019-05-10 13:14:08 -0400

    first commit

---
+++ create_project.sh
@@ -0,0 +1,12 @@
+skeleton=$1
+target=$2
+project_name=$3
+git clone $skeleton $3
+cd $3
+git remote remove origin
+git remote add origin $target
+git checkout --orphan develop
+git add --all
+git commit -m'first commit'
+git push --set-upstream origin develop
+
